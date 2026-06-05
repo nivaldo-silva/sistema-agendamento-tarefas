58a7eff893a35ee5b78eee56e2645fa5b0100427
Nivaldo Silva <nivaldosilva.contato@gmail.com>
2026-06-05 11:09:14 -0300

    Fix: reorganiza estrutura de pastas e remove duplicatas antigas

---
+++ cadastro-usuarios/src/main/java/io/github/nivaldosilva/cadastro_usuarios/infrastructure/startup/AdminUserInitializer.java
@@ -0,0 +1,56 @@
+package io.github.nivaldosilva.cadastro_usuarios.infrastructure.startup;
+
+import io.github.nivaldosilva.cadastro_usuarios.application.dto.request.EnderecoRequest;
+import io.github.nivaldosilva.cadastro_usuarios.application.dto.request.RegistroUsuarioRequest;
+import io.github.nivaldosilva.cadastro_usuarios.application.dto.request.TelefoneRequest;
+import io.github.nivaldosilva.cadastro_usuarios.infrastructure.repository.UsuarioRepository;
+import io.github.nivaldosilva.cadastro_usuarios.application.service.UsuarioService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+import java.util.List;
+
+@Component
+@RequiredArgsConstructor
+@Slf4j
+public class AdminUserInitializer implements CommandLineRunner {
+
+	private final UsuarioService usuarioService;
+	private final UsuarioRepository usuarioRepository;
+
+	@Override
+	public void run(String... args) throws Exception {
+		if (usuarioRepository.findByEmail("admin@email.com").isEmpty()) {
+
+			EnderecoRequest enderecoAdminRequest = new EnderecoRequest(
+					"N/A",
+					"N/A",
+					"N/A",
+					"N/A",
+					"N/A",
+					"NA",
+					"00000-000"
+			);
+
+			TelefoneRequest telefoneAdminRequest = new TelefoneRequest(
+					"000000000",
+					"000"
+			);
+
+			RegistroUsuarioRequest adminRequest = new RegistroUsuarioRequest(
+					"Admin",
+					"admin@email.com",
+					"admin123",
+					List.of(enderecoAdminRequest),
+					List.of(telefoneAdminRequest)
+			);
+
+
+			usuarioService.criarAdmin(adminRequest);
+			log.info("Usuario administrador padrao criado.");
+		} else {
+			log.info("O usuario administrador padrao ja existe..");
+		}
+	}
+}
